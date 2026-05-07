6de51aa1797095769afdc5bcc2c8c23c207e7658
guilherme <guiguicaiano10@gmail.com>
2026-05-07 11:32:57 -0300

    feat: configurando o menu

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,21 @@ window.onload = function(){
     },3000)
 
 }
+
+// MENU HAMBURGUER
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const menuIcone= document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");     
+        }
+    }
+})
+
+
 function trocar(cor){
     document.body.style.background=cor;
 }
